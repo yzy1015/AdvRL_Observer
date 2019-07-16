341f8a6ed46a2303554d86cabbf8924f1d7735e5
yzy1015 <zhyyang1015@gmail.com>
2019-07-15 20:57:16 -0400

    Add files via upload

---
+++ std4/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python3 seq1.py &
+
+python3 seq2.py &
+
+python3 seq3.py &
+
+python3 seq4.py &
+
+python3 seq5.py &
+
+python3 sim1.py &
+
+python3 sim2.py &
+
+python3 sim3.py &
+
+python3 sim4.py &
+
+python3 sim5.py &
